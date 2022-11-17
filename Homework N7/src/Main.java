301565d3fa3817cc83e034107b1424adc1a71718
Gogokhiag12 <GiorgiGogokhia12@gmail.com>
2022-11-17 14:43:25 +0400

    exercise N1 countPositives

---
+++ Homework N7/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private static int countPositives(int[] a){
+        int count = 0;
+        for (int positive: a){
+            if (positive > 0)
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, -3, -7, 3, 6, 8, 9};
+        System.out.println(countPositives(array));
+
+    }
+}
