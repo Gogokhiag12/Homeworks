e2e4bcf2e6d57189c64ee953c665a114c1a09e7d
Gogokhiag12 <GiorgiGogokhia12@gmail.com>
2022-11-17 14:58:42 +0400

    exercise N2 countNegatives

--- Homework N7/src/Main.java
+++ Homework N7/src/Main.java
@@ -9,9 +9,19 @@ public class Main {
         return count;
     }
 
+    private static int countNegatives(int[] a){
+        int count = 0;
+        for (int positive: a){
+            if (positive < 0)
+                count++;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] array = {4, -3, -7, 3, 6, 8, 9};
         System.out.println(countPositives(array));
+        System.out.println(countNegatives(array));
 
     }
 }
