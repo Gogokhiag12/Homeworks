86c06766e283f0120e028304cbd7ef437ad5db3a
Gogokhiag12 <GiorgiGogokhia12@gmail.com>
2022-11-17 15:02:37 +0400

    exercise N4 negativesSum

--- Homework N7/src/Main.java
+++ Homework N7/src/Main.java
@@ -27,12 +27,22 @@ public class Main {
         return count;
     }
 
+    private static int negativesSum(int[] a) {
+        int count = 0;
+        for (int positive : a) {
+            if (positive < 0)
+                count += positive;
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         int[] array = {4, -3, -7, 3, 6, 8, 9};
         System.out.println(countPositives(array));
         System.out.println(countNegatives(array));
         System.out.println(positivesSum(array));
+        System.out.println(negativesSum(array));
 
     }
 }
