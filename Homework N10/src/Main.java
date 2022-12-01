e74d5d10c8d769af4a2f884ef805fca08a706f29
Gogokhiag12 <GiorgiGogokhia12@gmail.com>
2022-12-01 15:01:23 +0400

    Exercise N2

--- Homework N10/src/Main.java
+++ Homework N10/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     /*
     Function name - listIntegers
-    Function changes "alone" element with higher element between its before and after elements
+    Function replaces "alone" element by larger element between its before and after elements
      */
     private static void listIntegers() {
         int n;
@@ -26,11 +26,33 @@ public class Main {
         }
         System.out.println("Your array after changes is: " + Arrays.toString(array));
     }
+    /*
+    Function name: zeroMax
+    Functions replaces all zeros by the largest odd element to the right of the zero
+     */
+    private static int[] zeroMax(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j <= nums.length - 1; j++) {
+                    if ( nums[j] > max && nums[j] % 2 == 1 )
+                        max = nums[j];
+                }
+                nums[i] = max;
+                max = 0;
+            }
+        }
+        return nums;
+    }
 
     public static void main(String[] args) {
-        System.out.println(" - - - - - - - First Exercise - - - - - - - ");
-        listIntegers();
+//        System.out.println(" - - - - - - - First Exercise - - - - - - - ");
+//        listIntegers();
         System.out.println(" - - - - - - - Second Exercise - - - - - - - ");
+        int[] nums = {16, 0, 3, 0, 4, 0, 6, 0, 15};
+        System.out.println("Your array before changes: " + Arrays.toString(nums));
+        System.out.println("Your array after changes: " + Arrays.toString(zeroMax(nums)));
+
         System.out.println(" - - - - - - - Third Exercise - - - - - - - ");
     }
 }
